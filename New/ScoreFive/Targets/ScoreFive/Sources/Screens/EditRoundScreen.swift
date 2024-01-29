645fa5f5c02e777d420ea6b6384691d7104f2473
Varun Santhanam <talkto@vsanthanam.com>
2024-01-29 15:16:27 +0530

    Duplicate project using Tuist in /New (#15)

---
+++ New/ScoreFive/Targets/ScoreFive/Sources/Screens/EditRoundScreen.swift
@@ -0,0 +1,163 @@
+// ScoreFive
+// EditRoundScreen.swift
+//
+// MIT License
+//
+// Copyright (c) 2021 Varun Santhanam
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the  Software), to deal
+//
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED  AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import FiveKit
+import SwiftUI
+
+struct EditRoundView: View {
+
+    init(
+        scoreCard: Binding<ScoreCard>,
+        editingRound: ScoreCard.Round?,
+        players: [String]
+    ) {
+        _scoreCard = scoreCard
+        self.editingRound = editingRound
+        self.players = players
+    }
+
+    // MARK: - View
+
+    @ViewBuilder
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    ForEach(players, id: \.self) { player in
+                        TextField(player, value: $scores.animation()[player], format: .number)
+                            .focused($focus, equals: player)
+                            .keyboardType(.numberPad)
+                    }
+                } footer: {
+                    if !areScoresLegal {
+                        Text("Scores must be between 0 and 50")
+                            .foregroundStyle(Color.red)
+                    } else if !containsAllScores {
+                        Text("Enter a score for every player")
+                    } else if !hasWinner {
+                        Text("At least one player must have a score of 0")
+                    } else if !hasLoser {
+                        Text("At least one player must have a score greater than 0")
+                    } else if !canSave {
+                        Text("Cannot change an existing score by this much. Delete newer scores first.")
+                            .foregroundStyle(Color.red)
+                    } else {
+                        EmptyView()
+                    }
+                }
+                if canSave {
+                    Section {
+                        Button(action: save) {
+                            Text("Save")
+                                .frame(maxWidth: .infinity)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                ToolbarItem {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+        .onAppear {
+            if let editingRound {
+                for player in players {
+                    scores[player] = editingRound[player]
+                }
+            }
+            focus = players[0]
+        }
+    }
+
+    // MARK: - Private
+
+    private let editingRound: ScoreCard.Round?
+    private let players: [String]
+
+    @Binding
+    private var scoreCard: ScoreCard
+
+    @FocusState
+    private var focus: String?
+
+    @Environment(\.dismiss)
+    private var dismiss: DismissAction
+
+    @State
+    private var scores: [String: Int] = [:]
+
+    private var title: String {
+        editingRound == nil ? "Add Scores" : "Edit Scores"
+    }
+
+    private var containsAllScores: Bool {
+        scores.values.count == players.count
+    }
+
+    private var areScoresLegal: Bool {
+        scores.values.allSatisfy(isLegalScore)
+    }
+
+    private var hasWinner: Bool {
+        scores.values.contains(0)
+    }
+
+    private var hasLoser: Bool {
+        !scores.values.filter { score in score != 0 }.isEmpty
+    }
+
+    private var canSave: Bool {
+        guard containsAllScores, areScoresLegal, hasWinner, hasLoser else {
+            return false
+        }
+        if let editingRound {
+            var newRound = ScoreCard.Round(players: players, id: editingRound.id)
+            for player in players {
+                newRound[player] = scores[player]
+            }
+            return scoreCard.canReplaceRound(id: editingRound.id, withRound: newRound)
+        } else {
+            return true
+        }
+    }
+
+    private func save() {
+        var round = ScoreCard.Round(players: players)
+        for player in players {
+            round[player] = scores[player]
+        }
+        if let editingRound {
+            scoreCard.replaceRound(id: editingRound.id, withRound: round)
+        } else {
+            scoreCard.addRound(round)
+        }
+        dismiss()
+    }
+
+}
---
+++ New/ScoreFive/Targets/ScoreFive/Sources/Screens/RecordScreen.swift
@@ -0,0 +1,236 @@
+// ScoreFive
+// RecordScreen.swift
+//
+// MIT License
+//
+// Copyright (c) 2021 Varun Santhanam
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the  Software), to deal
+//
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED  AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import FiveKit
+import SwiftUI
+import SwiftUtilities
+
+@MainActor
+struct RecordView: View {
+
+    // MARK: - Initializers
+
+    init(activeRecord: Record) {
+        self.activeRecord = activeRecord
+    }
+
+    // MARK: - View
+
+    @ViewBuilder
+    var body: some View {
+        VStack(spacing: 0.0) {
+            playerNamesHeader
+            Spacer()
+                .frame(maxWidth: .infinity, minHeight: 1.0, maxHeight: 1.0)
+            roundsList
+            totalScoresFooter
+        }
+        .navigationTitle("Score Card")
+        .navigationBarTitleDisplayMode(.inline)
+        .listStyle(.plain)
+        .sheet(isPresented: $addingRound) {
+            EditRoundView(
+                scoreCard: $activeRecord.scoreCard,
+                editingRound: nil,
+                players: activeRecord.scoreCard.alivePlayers
+            )
+        }
+        .sheet(item: $editingRound, id: \.self) { round in
+            EditRoundView(
+                scoreCard: $activeRecord.scoreCard,
+                editingRound: round,
+                players: round.players
+            )
+        }
+    }
+
+    // MARK: - Private
+
+    @Bindable
+    private var activeRecord: Record
+
+    @State
+    private var addingRound = false
+
+    @State
+    private var editingRound: ScoreCard.Round? = nil
+
+    @ViewBuilder
+    private var roundsList: some View {
+        List {
+            ForEach(activeRecord.scoreCard.rounds) { round in
+                Button {
+                    editingRound = round
+                } label: {
+                    RecordRow {
+                        Text(activeRecord.scoreCard.startingPlayer(atIndex: activeRecord.scoreCard.rounds.firstIndex(of: round) ?? 0).playerSignpost)
+                            .fontDesign(.monospaced)
+                    } content: {
+                        let scores = activeRecord.scoreCard.players
+                            .map { player in
+                                round[player]
+                            }
+                        let aliveScores = activeRecord.scoreCard.alivePlayers
+                            .compactMap { player in
+                                round[player]
+                            }
+                        EntriesView(
+                            values: scores,
+                            isWinner: { value in
+                                value == aliveScores.min()
+                            },
+                            isLoser: { value in
+                                value == aliveScores.max()
+                            }
+                        )
+                    }
+                    .background(((activeRecord.scoreCard.rounds.firstIndex(of: round) ?? 0) % 2 == 0) ? Color.systemBackground : Color.secondarySystemBackground)
+                }
+                .deleteDisabled(!activeRecord.scoreCard.canRemoveRound(id: round.id))
+            }
+            .onDelete { indexSet in
+                for index in indexSet {
+                    activeRecord.scoreCard.removeRound(atIndex: index)
+                }
+            }
+            .listRowInsets(.init())
+            .listRowSeparator(.hidden)
+            .recordRowConfiguration(hasTopDivider: true, hasBottomDivider: true)
+            if activeRecord.scoreCard.alivePlayers.count > 1 {
+                addScoresButton
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var addScoresButton: some View {
+        Button {
+            addingRound.toggle()
+        } label: {
+            RecordRow {
+                Text(activeRecord.scoreCard.startingPlayer(atIndex: activeRecord.scoreCard.rounds.count).playerSignpost)
+                    .frame(width: 44.0, height: 44.0)
+                    .opacity(0.3)
+            } content: {
+                Text("Add Scores")
+                    .opacity(0.3)
+            }
+            .recordRowConfiguration(hasTopDivider: true, hasBottomDivider: true)
+        }
+        .listRowInsets(.init())
+        .listRowSeparator(.hidden)
+    }
+
+    @ViewBuilder
+    private var playerNamesHeader: some View {
+        RecordRow {
+            EntriesView(
+                values: activeRecord.scoreCard.players,
+                toString: \.playerSignpost,
+                isEliminated: { player in
+                    !activeRecord.scoreCard.alivePlayers.contains(player)
+                },
+                isAccented: { _ in
+                    true
+                }
+            )
+        }
+        .recordRowConfiguration(hasTopDivider: true, hasBottomDivider: true)
+    }
+
+    @ViewBuilder
+    private var totalScoresFooter: some View {
+        RecordRow {
+            let scores = activeRecord.players.map(activeRecord.scoreCard.totalScore(forPlayer:))
+            EntriesView(
+                values: scores,
+                isWinner: { value in
+                    value == activeRecord.scoreCard.lowestScore
+                },
+                isLoser: { value in
+                    value == activeRecord.scoreCard.highestScore
+                },
+                isAccented: { _ in
+                    true
+                }
+            )
+        }
+        .recordRowConfiguration(hasTopDivider: true, hasBottomDivider: true)
+    }
+
+}
+
+private extension String {
+    var playerSignpost: String {
+        let comps = split(separator: " ")
+        if comps.count == 2, comps[0].lowercased() == "player", Int(comps[1]) != nil {
+            return "P" + comps[1]
+        } else {
+            return first?.uppercased() ?? "X"
+        }
+    }
+}
+
+private extension ScoreCard.Round {
+    private var lowestScore: Int {
+        players
+            .compactMap { player in
+                self[player]
+            }
+            .min() ?? 0
+    }
+
+    private var highestScore: Int {
+        players
+            .compactMap { player in
+                self[player]
+            }
+            .max() ?? 50
+    }
+}
+
+private extension ScoreCard {
+
+    var lowestScore: Int {
+        alivePlayers
+            .compactMap { player in
+                self[player]
+            }
+            .min() ?? 0
+    }
+
+    var highestScore: Int {
+        alivePlayers
+            .compactMap { player in
+                self[player]
+            }
+            .max() ?? 0
+    }
+
+}
+
+#Preview {
+    RecordView(activeRecord: .init(scoreCard: .init(players: ["Player 1", "Player 2"], scoreLimit: 250)))
+}

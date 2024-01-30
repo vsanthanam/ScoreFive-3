a252b9b15b6f1d435b0083a80605173eef590094
Varun Santhanam <talkto@vsanthanam.com>
2024-01-30 10:29:27 +0530

    Rename FiveKitTests.swift (#20)

--- Packages/FiveKit/Tests/FiveKitTests/FiveKitTests.swift
+++ Packages/FiveKit/Tests/FiveKitTests/FiveKitTests.swift
@@ -1,5 +1,5 @@
 // ScoreFive
-// FiveTests.swift
+// FiveKitTests.swift
 //
 // MIT License
 //
@@ -26,12 +26,6 @@
 @testable import FiveKit
 import XCTest
 
-final class FiveTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+final class FiveKitTests: XCTestCase {
+    func testExample() throws {}
 }
